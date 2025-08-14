16467b172d15f2bbd59b8dac7e4f19202bdf7dc4
atikur40 <atikurrahman42607@gmail.com>
2025-08-14 11:34:17 +0600

    update
    update

---
+++ vendor/kreativdev/installer/src/config/kdinstaller.php
@@ -0,0 +1,12 @@
+<?php 
+
+    return [
+        'item_name' => "Carlist",
+        "item_id" => 47848444,
+        "vension" => 2.0,
+        "released_on" => "18th March, 2024",
+        "broadcast_driver" => "log",
+        "email_api" => 'https://kreativdev.com/emailcollector/api/collect'
+    ];
+
+?>
